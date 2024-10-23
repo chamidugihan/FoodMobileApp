c9f8b21085016c97979ebce012d47615ea40be65
chamidugihan <w.g.c.g.ellepola@gmail.com>
2024-10-23 20:18:20 +0530

    edit the menuscreen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,36 +1,13 @@
-import Colors from '@/src/constants/Colors';
-import { Image, StyleSheet,Text,View } from 'react-native';
+import { View } from 'react-native';
 import products from '@/assets/data/products';
+import ProductItemList from '@/src/components/productItemList';
 
-const product = products[0];
 
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Image source={{uri:product.image}} style={styles.img}/>
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
-    </View>
-  );
+export default function MenuScreen() {
+  return (<View>
+    <ProductItemList product={products[5]}/>
+    <ProductItemList product={products[4]}/>
+  </View>);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor:"white",
-    padding:10,
-    borderRadius:10
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical:10,
-  },
-  price :{
-    color:Colors.light.tint,
-    fontWeight:"bold",
-  },
-  img:{
-    width:"100%",
-    aspectRatio:1,
-  }
-});
+
---
+++ src/components/productItemList.tsx
@@ -0,0 +1,34 @@
+import Colors from '@/src/constants/Colors';
+import { Image, StyleSheet,Text,View } from 'react-native';
+
+const ProductItemList = ({product}) =>{
+  return(
+    <View style={styles.container}>
+    <Image source={{uri:product.image}} style={styles.img}/>
+    <Text style={styles.title}>{product.name}</Text>
+    <Text style={styles.price}>${product.price}</Text>
+  </View>
+  );
+};
+
+export default ProductItemList;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor:"white",
+    padding:10,
+    borderRadius:10
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical:10,
+  },
+  price :{
+    color:Colors.light.tint,
+    fontWeight:"bold",
+  },
+  img:{
+    width:"100%",
+    aspectRatio:1,
+  }
+});
